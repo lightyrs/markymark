b125c0549b5a3bf98acfa4e7f442c60280077bcd
Harris Novick <harrisdavidnovick@gmail.com>
2014-03-25 22:24:14 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Markymark::Application.config.secret_key_base = '903d1f7b4570021924c57310b035790b3acb81eaa37793e910ef7e1a4102e76fa4846bdf2328aae02e8b6e57d3b41a1d1cb1aa1b5426ed09dd5dd18fc1bc648a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Markymark::Application.config.session_store :cookie_store, key: '_markymark_session'
