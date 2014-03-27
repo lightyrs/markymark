f3a1b29f2d243246afeb53604ebb8637c9719313
Harris Novick <harrisdavidnovick@gmail.com>
2014-03-27 02:53:15 -0400

    don't retry harvest job

--- app/workers/harvest_links_worker.rb
+++ app/workers/harvest_links_worker.rb
@@ -2,7 +2,7 @@ class HarvestLinksWorker
 
   include Sidekiq::Worker
 
-  sidekiq_options queue: :normal_priority
+  sidekiq_options queue: :normal_priority, retry: false
 
   def perform(user_id, provider)
     reaper = Reaper.new(user_id: user_id, provider: provider)
