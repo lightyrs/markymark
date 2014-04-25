b7adc4d53968244c6c05750a44d8fefc111422c4
Harris Novick <harrisdavidnovick@gmail.com>
2014-04-24 20:56:20 -0400

    minor improvements

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,19 @@ Markymark::Application.routes.draw do
 
   resources :users, only: [ :show, :edit, :update ]
 
-  resources :links, only: [ :index, :show ] do
-    get 'tags', on: :collection
-  end
-  get '/links/tags/:tag' => 'links#index', as: 'tagged_links'
-  get '/links/domains/(:site)' => 'links#index', as: 'domain_links', constraints: { site: /[^\/]+/ }
-
   get '/auth/:provider/callback' => 'sessions#create'
   get '/login/:provider' => 'sessions#new', as: :login
   get '/logout' => 'sessions#destroy', as: :logout
   get '/auth/failure' => 'sessions#failure'
+
+  resources :links, only: [ :index, :show ] do
+    get 'tags', on: :collection
+  end
+
+  get '/links/tags/:tag' => 'links#index', as: 'tagged_links'
+  get '/links/domains/(:site)' => 'links#index', as: 'domain_links', constraints: { site: /[^\/]+/ }
+
+  get '/admin' => 'admin#index'
 
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
