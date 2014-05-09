5a0e9b18e41d355429e5c8d8f950bd0f7c73df78
Harris Novick <harrisdavidnovick@gmail.com>
2014-05-09 19:40:05 -0400

    :thumbsup: :neckbeard: :shipit:

--- app/workers/scrape_link_group_worker.rb
+++ app/workers/scrape_link_group_worker.rb
@@ -5,6 +5,8 @@ class ScrapeLinkGroupWorker
   sidekiq_options queue: :normal_priority, retry: 2
 
   def perform(link_group)
-    Scraper.make_request(link_group) rescue false
+    ActiveRecord::Base.connection_pool.with_connection do
+      Scraper.make_request(link_group) rescue false
+    end
   end
 end
--- app/workers/scrape_links_worker.rb
+++ app/workers/scrape_links_worker.rb
@@ -5,6 +5,8 @@ class ScrapeLinksWorker
   sidekiq_options queue: :high_priority, retry: 2
 
   def perform(user_id, provider_id)
-    Link.scrape(user_id, provider_id)
+    ActiveRecord::Base.connection_pool.with_connection do
+      Link.scrape(user_id, provider_id) rescue false
+    end
   end
 end
